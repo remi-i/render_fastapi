d5a5c0ac0ec682396b44d8fbc528731a03b704ae
remi-i <adokado1024@gmail.com>
2026-06-22 14:06:22 +0900

    random

--- main.py
+++ main.py
@@ -1,42 +1,5 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-import random
+from import random
 
-app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/omikuji")
-def omikuji():
-    omikuji_list = [
-        "大吉",
-        "中吉",
-        "小吉",
-        "吉",
-        "半吉",
-        "末吉",
-        "末小吉",
-        "凶",
-        "小凶",
-        "大凶"
-    ]
-
-    return {"result": omikuji_list[random.randrange(10)]}
-
-@app.get("/index")
-def index():
-    html_content = """
-    <html>
-        <head>
-            <title>ホームページTOP</title>
-        </head>
-        <body>
-            <h1>ようこそ！</h1>
-            <p>岩田です。</p>
-            <p>FastAPIの課題で作成したページです。</p>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
+# 0.0以上1.0未満のランダムな浮動小数点数を生成
+random_number = random.random()
+print("Random float number:", random_number)
